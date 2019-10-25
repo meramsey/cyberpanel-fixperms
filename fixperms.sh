18f42bcf4357064ab5037759be790b9907031190
Michael Ramsey <mike@hackerdise.me>
2019-10-25 10:53:18 +0000

    Add new file

---
+++ fixperms.sh
@@ -0,0 +1,169 @@
+#! /bin/bash
+# Cyberpanel Fix Perms
+# 
+# 
+# Forked from https://github.com/PeachFlame/cPanel-fixperms
+#
+
+# Set verbose to null
+verbose=""
+
+
+#Print the help text
+helptext () {
+    tput bold
+    tput setaf 2
+    echo "Fix perms script help:"
+    echo "Sets file/directory permissions to match suPHP and FastCGI schemes"
+    echo "USAGE: fixperms [options] -a account_name"
+    echo "-------"
+    echo "Options:"
+    echo "-h or --help: print this screen and exit"
+    echo "-v: verbose output"
+    echo "-all: run on all cPanel accounts"
+    echo "--account or -a: specify a Cyberpanel account"
+#   echo "--domain or -d: specify a Cyberpanel domain"
+    tput sgr0
+    exit 0
+}
+
+# Main workhorse, fix perms per account passed to it
+fixperms () {
+
+  #Get account from what is passed to the function
+  account=$1
+
+  #Make sure account isn't blank
+  if [ -z "$account" ]
+  then
+    tput bold
+    tput setaf 1
+    echo "Need an account name!"
+    tput sgr0
+    helptext
+  #Else, start doing work
+  else
+
+    #Get the account's homedir
+    HOMEDIR=$(egrep "^${account}:" /etc/passwd | cut -d: -f6)
+
+    tput bold
+    tput setaf 4
+    echo "Fixing perms for $account:"
+    tput setaf 3
+    echo "------------------------"
+    tput setaf 4
+    echo "Fixing website files...."
+    tput sgr0
+    
+    #Fix individual files in public_html
+    find "$HOMEDIR"/public_html -type d -exec chmod $verbose 755 {} \;
+    find "$HOMEDIR"/public_html -type f | xargs -d$'\n' -r chmod $verbose 644
+    find "$HOMEDIR"/public_html -name '*.cgi' -o -name '*.pl' | xargs -r chmod $verbose 755
+    chown $verbose -R "$account":"$account" "$HOMEDIR"/public_html/*
+    find "$HOMEDIR"/* -name .htaccess -exec chown $verbose "$account"."$account" {} \;
+
+    tput bold
+    tput setaf 4
+    echo "Fixing public_html...."
+    tput sgr0
+    #Fix perms of public_html itself
+    chown $verbose "$account":"$account" "$HOMEDIR"/public_html
+    chmod $verbose 755 "$HOMEDIR"/public_html
+
+    tput bold
+    tput setaf 4
+    echo "Fixing logs...."
+    tput sgr0
+    #Fix perms of $HOMEDIR/logs
+    chown $verbose nobody:"$account" "$HOMEDIR"/logs
+    chmod $verbose 750 "$HOMEDIR"/logs
+    find "$HOMEDIR"/logs/* -name '*.access_log' -exec chown $verbose nobody."$account" {} \;
+
+    tput bold
+    tput setaf 4
+    echo "Fixing mailperms...."
+    tput sgr0
+    #Fix perms of /home/vmail
+    chown -R vmail:vmail /home/vmail
+    chmod 755 /home/vmail
+    find /home/vmail -type d -exec chmod 0755 {} \;
+    find /home/vmail -type f -exec chmod 0640 {} \;
+
+    #Fix subdomains that lie outside of public_html
+    #tput setaf 3
+    #tput bold
+    #echo "------------------------"
+    #tput setaf 4
+    #echo "Fixing any domains with a document root outside of public_html...."
+    #for SUBDOMAIN in $(grep -i documentroot /var/cpanel/userdata/$account/* | grep -v '.cache\|_SSL' | awk '{print $2}' | grep -v public_html)
+    #do
+      #tput bold
+      #tput setaf 4
+      #echo "Fixing sub/addon domain document root $SUBDOMAIN...."
+      #tput sgr0
+      #find $SUBDOMAIN -type d -exec chmod $verbose 755 {} \;
+      #find $SUBDOMAIN -type f | xargs -d$'\n' -r chmod $verbose 644
+      #find $SUBDOMAIN -name '*.cgi' -o -name '*.pl' | xargs -r chmod $verbose 755
+      #chown $verbose -R $account:$account $SUBDOMAIN
+      #find $SUBDOMAIN -name .htaccess -exec chown $verbose $account.$account {} \;
+    #done
+
+  #Finished
+    tput bold
+    tput setaf 3
+    echo "Finished!"
+    echo "------------------------"
+    printf "\n\n"
+    tput sgr0
+  fi
+
+  return 0
+}
+
+#Parses all users through Cyberpanel's users file
+all () {
+    for user in $(cut -d: -f1 /etc/domainusers)
+    do
+  fixperms "$user"
+    done
+}
+
+#Main function, switches options passed to it
+case "$1" in
+
+    -h) helptext
+  ;;
+    --help) helptext
+      ;;
+    -v) verbose="-v"
+
+  case "$2" in
+
+    -all) all
+           ;;
+    --account) fixperms "$3"
+         ;;
+    -a) fixperms "$3"
+        ;;
+    *) tput bold
+           tput setaf 1
+       echo "Invalid Option!"
+       helptext
+       ;;
+  esac
+  ;;
+
+    -all) all
+    ;;
+    --account) fixperms "$2"
+          ;;
+    -a) fixperms "$2"
+  ;;
+    *)
+       tput bold
+       tput setaf 1
+       echo "Invalid Option!"
+       helptext
+       ;;
+esac
